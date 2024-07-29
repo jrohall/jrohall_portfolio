f204673c8310924eaa237d1d330061e1b3051224
Jeremiah Rohall <103393035+jrohall@users.noreply.github.com>
2024-07-29 16:18:03 -0400

    added new blog page and more stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AbComponent } from './ab/ab.component';
 import { ProjComponent } from './proj/proj.component';
 import { DevComponent } from './dev/dev.component';
 import { YapComponent } from './yap/yap.component';
+import { HabsComponent } from './posts/habs/habs.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'exp', pathMatch: 'full' },
@@ -12,6 +13,7 @@ export const routes: Routes = [
     {path: 'proj', component: ProjComponent },
     {path: 'dev', component: DevComponent },
     {path: 'yap', component: YapComponent },
+    {path: 'habsblog', component: HabsComponent},
     {path: '**', redirectTo: 'exp'},
 ];
 
